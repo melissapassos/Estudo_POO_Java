d983da3bb4bcf951da5a7a69e88fc504f58137a9
Melissa Passos <melissa.padilha523@gmail.com>
2018-09-22 16:06:03 -0300

    Exercício da Aula prática 1 finalizado.

--- AulaPratica1/src/modelo/Agenda.java
+++ AulaPratica1/src/modelo/Agenda.java
@@ -1,5 +1,26 @@
 package modelo;
 
+import java.util.Date;
+
 public class Agenda {
 
+	Date data;
+	Animal animal;
+	
+	public Agenda() {
+
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Animal getAnimal() {
+		return animal;
+	}
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+	
 }
--- AulaPratica1/src/principal/Principal.java
+++ AulaPratica1/src/principal/Principal.java
@@ -7,10 +7,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Cliente cliente = new Cliente("Joao", "Aqui", "555-1234");
+		Cliente cliente = new Cliente("João", "Rua das Orquídeas", "555-1234");
 		Animal animal = new Animal ("Toto", "Cachorro", "SRD", cliente);
-	
-		System.out.println("Nome:" + animal.getTutor().getNome() + ", Animal:" + animal.getNome());
+		
+		System.out.println("Nome: " + animal.getTutor().getNome() + "\nEndereço: " + animal.getTutor().getEndereco() + "\nTelefone: " + animal.getTutor().getTelefone() + "\nAnimal: " + animal.getNome() + "\nTipo: " + animal.getTipo() + "\nRaça: " + animal.getRaca());
 	}
 
 }
